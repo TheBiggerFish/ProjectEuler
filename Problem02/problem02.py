3c83b7dc3d64128d416acecf184d52a645b3c41e
TheBiggerFish <clhnumber4@gmail.com>
2020-11-09 18:05:29 -0500

    Formatting problem 2

--- Problem02/problem02.py
+++ Problem02/problem02.py
@@ -6,7 +6,6 @@
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
 
-
 def fib_sum_even(max_value):
     cur = 1
     prev = 1
