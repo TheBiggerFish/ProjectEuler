d012e90141ca4f2f5fc34cba095fb30606ca43d7
TheBiggerFish <clhnumber4@gmail.com>
2020-11-10 09:29:49 -0500

    Submitted problem 9

---
+++ Problem09/problem09.py
@@ -0,0 +1,20 @@
+# Written by Cameron Haddock
+# Submitted as a solution to Project Euler's Problem 6
+
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+
+import math
+
+def triplet(sum_):
+    for i in range(1,int(sum_/3)):
+        for j in range(i,int(sum_/2)):
+            for k in range(sum_-(i+j),sum_):
+                if i*i + j*j == k*k and i+j+k == sum_:
+                    return (i,j,k)
+
+
+print(math.prod(triplet(100)))
