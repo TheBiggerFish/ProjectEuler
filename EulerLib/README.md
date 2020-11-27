e564258234e304351c771bb232cf4b2f16a78443
TheBiggerFish <clhnumber4@gmail.com>
2020-11-27 00:17:36 -0500

    Starting EulerLib library off with fraction module Module includes Fraction class and ContinuedFraction class

---
+++ EulerLib/README.md
@@ -0,0 +1,3 @@
+# EulerLib
+
+This is a library of python classes and functions that I use in solving problems for ProjectEuler
---
+++ EulerLib/fraction.py
@@ -0,0 +1,134 @@
+from math import lcm,gcd
+
+class Fraction:
+    def __init__(self,numer,denom=1):
+        self.n = numer
+        self.d = denom
+    
+    def evaluate(self):
+        return self.n / self.d
+
+    def reciprocal(self):
+        return Fraction(self.d,self.n)
+    
+    def add_int(self,num):
+        return Fraction(self.n + (num * self.d),self.d)
+
+    def __mul__(self,other):
+        return Fraction(self.n*other.n,self.d*other.d)
+    
+    def __add__(self,other):
+        lcd = Fraction.lcd(self,other)
+        self_n = self.n * (lcd // self.d)
+        other_n = other.n * (lcd // other.d)
+        return Fraction(self_n + other_n,lcd)
+
+    def __str__(self):
+        return str(self.n) + '/' + str(self.d)
+
+    @staticmethod
+    def lcd(f1,f2):
+        return lcm(f1.d,f2.d)
+
+    def reduce(self):
+        div = gcd(self.n,self.d)
+        return Fraction(self.n//div,self.d//div)
+
+
+
+
+class ContinuedFraction:
+    def __init__(self,addend=0,numer=0,denom=None):
+        self.a = addend #type:int
+        self.n = numer #type:int
+        self.d = denom #type:ContinuedFraction
+        
+    @staticmethod
+    # Generate continued fraction using a continued fraction representation sequence.
+    # The value x is used to generate the numerators of the continued fraction
+    def gen_from_seq(seq,x=2):
+        if len(seq) == 0:
+            return ContinuedFraction(addend=0)
+
+        frac = ContinuedFraction(addend=seq[-1])
+        for num in reversed(seq[:-1]):
+            if not isinstance(num,int) and not isinstance(num,float):
+                raise TypeError
+            frac.d = frac.copy()
+            frac.n = x-1
+            frac.a = num
+        return frac
+    
+    # Determines the representation sequence of a continued fraction
+    def get_seq(self):
+        seq = [self.a]
+        frac = self.d
+        while frac is not None:
+            seq.append(frac.a)
+            frac = frac.d
+        return seq
+
+    # Adds a continued fraction to the end of a continued fraction. 
+    def append(self,frac):
+        recurse = self
+        while recurse.d is not None:
+            recurse = recurse.d
+        recurse.d = frac
+        return self
+
+    def evaluate(self):
+        if self.n == 0 or self.d is None:
+            return float(self.a)
+        return self.a + (self.n / self.d.evaluate())
+
+    def reduce_fraction(self):
+        if self.n == 0 or self.d is None:
+            return Fraction(self.a)
+        return (Fraction(self.n) * self.d.reduce_fraction().reciprocal()).add_int(self.a)
+
+    @staticmethod
+    # Generate a continued fraction representation sequence of length l for a square root of number n
+    def gen_sqrt_seq(n,l):
+        if (n**0.5).is_integer():
+            return [n**0.5]
+        m = 0
+        d = 1
+        a = int(n**0.5)
+        seq = [a]
+        while len(seq) < l:
+            m = d * a - m
+            d = (n - (m**2)) // d
+            a = int(((n**0.5) + m) // d)
+            seq.append(a)
+        return seq
+
+    def copy(self):
+        return ContinuedFraction(addend=self.a,numer=self.n,denom=self.d)
+
+    @property
+    def a(self):
+        return self.__a
+        
+    @property
+    def n(self):
+        return self.__n
+    
+    @property
+    def d(self):
+        return self.__d
+
+    @a.setter
+    def a(self, a):
+        if not isinstance(a,int) and not isinstance(a,float):
+            raise TypeError
+        self.__a = a
+
+    @n.setter
+    def n(self, n):
+        if not isinstance(n,int) and not isinstance(n,float):
+            raise TypeError
+        self.__n = n
+
+    @d.setter
+    def d(self, d):
+        self.__d = d
