109d4f91ce41250efe719458b6e7b8af053db421
TheBiggerFish <clhnumber4@gmail.com>
2021-02-02 21:49:04 -0500

    Submitted problem 139
    Co-authored-by: Daniel <53064507+portobello-boy@users.noreply.github.com>

---
+++ Problem139/problem139.py
@@ -0,0 +1,50 @@
+# Written by Cameron Haddock & Daniel Millson
+# Submitted as a solution to Project Euler's Problem 139
+
+
+# Given that the perimeter of the right triangle is less than one-hundred million, how many Pythagorean triangles would allow such a tiling to take place?
+
+from math import gcd
+
+MAX_PERIMETER = 10**8
+
+UPPER_M = 5774
+LOWER_M = 1
+UPPER_N = 4083
+LOWER_N = 1
+
+valid_triangles = 0
+explored_triples = set()
+
+for n in range(LOWER_N, UPPER_N):
+    for m in range(n + 1, UPPER_M, 2):
+        if gcd(m, n) != 1:
+            continue
+
+        a = m**2 - n**2
+        b = 2*m*n
+        c = m**2 + n**2
+
+        if c % abs(a-b) != 0:
+            continue
+
+        if (a, b, c) in explored_triples:
+            continue
+
+        sum_ = a + b + c
+        k = 1
+        while k*sum_ < MAX_PERIMETER:
+            ka, kb, kc = k*a, k*b, k*c
+            
+            difference = abs(ka - kb)
+
+            if ka == 3 and kb == 4 and kc == 5 and kc % difference == 0:
+                print(ka, kb, kc, difference)
+
+            if kc % difference == 0:
+                valid_triangles += 1
+
+            explored_triples.add((ka, kb, kc))
+            k += 1
+
+print(valid_triangles)
