bd5485deae9ebf0875f8fb1076c40258471f6f23
TheBiggerFish <clhnumber4@gmail.com>
2023-08-25 13:47:33 -0400

    Formatting

--- Problem087/problem087.py
+++ Problem087/problem087.py
@@ -4,16 +4,16 @@
 # How many numbers below fifty million can be expressed as the sum of a prime square, prime cube, and prime fourth power?
 
 
-from fishpy.primes import sieve
+from fishpy.mathematics.primes import sieve
 
 
-def num_sums_below(sum_bound,prime_bound):
+def num_sums_below(sum_bound, prime_bound):
 
     primes = sieve(prime_bound)
-    primes_2 = set(filter(lambda x: x <= sum_bound**(1/2),primes))
-    primes_3 = set(filter(lambda x: x <= sum_bound**(1/3),primes))
-    primes_4 = set(filter(lambda x: x <= sum_bound**(1/4),primes))
-    
+    primes_2 = set(filter(lambda x: x <= sum_bound**(1/2), primes))
+    primes_3 = set(filter(lambda x: x <= sum_bound**(1/3), primes))
+    primes_4 = set(filter(lambda x: x <= sum_bound**(1/4), primes))
+
     found = set()
     for square in primes_2:
         for cube in primes_3:
@@ -22,4 +22,5 @@ def num_sums_below(sum_bound,prime_bound):
                     found.add(sum_)
     return len(found)
 
-print(num_sums_below(5*10**7,10000))
+
+print(num_sums_below(5*10**7, 10000))
