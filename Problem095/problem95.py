23f2304ab66d808afbcebabcb70b2a5ccfb79f0d
TheBiggerFish <clhnumber4@gmail.com>
2023-08-26 11:58:17 -0400

    Formatting problem 95

--- Problem095/problem95.py
+++ Problem095/problem95.py
@@ -1,45 +1,50 @@
+from functools import lru_cache
 from typing import Generator
-from math import sqrt
 
-def divisorListGenerator(n:int) -> Generator:
-    largerFactors = []
-    for i in range(1, int((n)+1)):
+
+def divisorGenerator(n: int) -> Generator[int, None, None]:
+    largerFactors: list[int] = []
+    for i in range(1, int((n**0.5)+1)):
         if n % i == 0:
             yield i
             if i*i != n:
                 largerFactors.append(int(n / i))
-    
+
     for f in reversed(largerFactors):
         yield f
     yield n
 
-def divisorList(n:int) -> set[int]:
-    return set(divisorListGenerator(n))
 
-def properDivisorList(n:int) -> set[int]:
-    divisors = divisorList(n)
+@lru_cache
+def properDivisors(n: int) -> set[int]:
+    divisors = set(divisorGenerator(n))
     divisors.remove(n)
     return divisors
 
-BOUND = 1_000_000
-seen: set[int] = set()
-longestChain: set[int] = set()
 
-for num in range(BOUND):
-    loop: set[int] = {num}
-    next_ = num
-    while True:
-        next_ = sum(properDivisorList(next_))
-        if next_ in seen or next_ >= BOUND:
-            break
-        if next_ == num:
-            print(loop)
-            if len(loop) > len(longestChain):
-                longestChain = loop
-            seen = seen | loop
-            break
-        if next_ in loop:
-            break    
-        loop.add(next_)
+def main():
+    BOUND = 1_000_000
+    seen: set[int] = set()
+    longestChain: set[int] = set()
 
-print(longestChain)
+    for num in range(BOUND):
+        loop: set[int] = {num}
+        next_ = num
+        while True:
+            next_ = sum(properDivisors(next_))
+            if next_ in seen or next_ >= BOUND:
+                break
+            if next_ == num:
+                print(loop)
+                if len(loop) > len(longestChain):
+                    longestChain = loop
+                seen = seen | loop
+                break
+            if next_ in loop:
+                break
+            loop.add(next_)
+    print(f'Smallest value in longest chain: {min(longestChain)}')
+
+
+if __name__ == "__main__":
+    main()
